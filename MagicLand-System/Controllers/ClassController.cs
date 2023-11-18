dcd1dc67fe11d99849032070c3cebf73a1b53ade
ThanhLee <lequanthanhbp2019@gmail.com>
2023-11-19 01:34:00 +0700

    ThanhLee push All API upto current DeadLine
    - Code has checked in normal way
    - Swagger tested in popular case
    - API run normal

--- MagicLand-System/Controllers/ClassController.cs
+++ MagicLand-System/Controllers/ClassController.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using MagicLand_System.Constants;
+using MagicLand_System.Domain.Models;
+using MagicLand_System.PayLoad.Response;
+using MagicLand_System.Services.Implements;
+using MagicLand_System.Services.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MagicLand_System.Controllers
@@ -6,8 +12,47 @@ namespace MagicLand_System.Controllers
     [ApiController]
     public class ClassController : BaseController<ClassController>
     {
-        public ClassController(ILogger<ClassController> logger) : base(logger)
+        private readonly IClassService _classService;
+        public ClassController(ILogger<ClassController> logger, IClassService classService) : base(logger)
         {
+            _classService = classService;
+        }
+        
+        [HttpGet(ApiEndpointConstant.ClassEnpoint.GetAll)]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetCourses()
+        {
+            var courses = await _classService.GetClassesAsync();
+            return Ok(courses);
+        }
+
+        [HttpGet(ApiEndpointConstant.ClassEnpoint.ClassByCourseId)]
+        [ProducesResponseType(typeof(ClassResponse), StatusCodes.Status200OK)]
+        [ProducesErrorResponseType(typeof(BadRequestObjectResult))]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetClassByCourseId(Guid id)
+        {
+            var courses = await _classService.GetClassesByCourseIdAsync(id);
+            return Ok(courses);
+        }
+
+        [HttpGet(ApiEndpointConstant.ClassEnpoint.ClassById)]
+        [ProducesResponseType(typeof(ClassResponse), StatusCodes.Status200OK)]
+        [ProducesErrorResponseType(typeof(BadRequestObjectResult))]
+        [AllowAnonymous]
+        public async Task<IActionResult> GetClassById(Guid id)
+        {
+            var courses = await _classService.GetClassById(id);
+            if(courses == null)
+            {
+                return BadRequest(new ErrorResponse
+                {
+                    Error = "Id Of Class Not Existed",
+                    StatusCode = StatusCodes.Status400BadRequest,
+                    TimeStamp = DateTime.Now,
+                });
+            }
+            return Ok(courses);
         }
     }
 }
---
+++ MagicLand-System/Mappers/CustomMapper/CustomMapper.cs
@@ -0,0 +1,66 @@
+﻿using MagicLand_System.Domain.Models;
+using MagicLand_System.Enums;
+using MagicLand_System.PayLoad.Response;
+
+namespace MagicLand_System.Mappers.CustomMapper
+{
+    public class CustomMapper
+    {
+        // Use to handel and support complicated mapping //
+        public static CourseResponse fromCourseToCourseResponse(Course course, IEnumerable<Course>? coursePrerequisites)
+        {
+            if (course == null)
+            {
+                return new CourseResponse();
+            }
+
+            CourseResponse response = new CourseResponse
+            {
+                Id = course.Id,
+                Name = course.Name,
+                NumberOfSession = course.NumberOfSession,
+                MinAgeStudent = course.MinYearOldsStudent,
+                MaxAgeStudent = course.MaxYearOldsStudent,
+                CoursePrerequisites = coursePrerequisites != null
+                ? coursePrerequisites.Select(c => fromCourseToCourseResponse(c, null)).ToList()
+                : new List<CourseResponse>()
+            };
+            return response;
+        }
+    
+        public static UserResponse fromUserToUserResponse(User user)
+        {
+            if (user == null)
+            {
+                return new UserResponse();
+            }
+            UserResponse response = new UserResponse
+            {
+                Id = user.Id,
+                FullName = user.FullName,
+                Phone = user.Phone,
+                Email = user.Email,
+                Gender = user.Gender,
+                DateOfBirth = user.DateOfBirth,
+                Address = fromAddressToAddressResponse(user.Address)
+            };
+            return response;
+        }
+
+        public static AddressResponse fromAddressToAddressResponse(Address address)
+        {
+            if (address == null)
+            {
+                return new AddressResponse();
+            }
+            AddressResponse response = new AddressResponse
+            {
+                Id = address.Id,
+                Street = address.Street,
+                District = address.District,
+                City = address.City
+            };
+            return response;
+        }
+    }
+}
