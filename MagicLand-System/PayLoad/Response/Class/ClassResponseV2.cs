d524a0c076de32ab0fb54e4fe253d9b3eeb982fc
Ngô Gia Thưởng <110913482+giathuong172@users.noreply.github.com>
2024-01-08 16:21:42 +0700

    [Thuong] update project

--- MagicLand-System/PayLoad/Response/Class/ClassResponseV2.cs
+++ MagicLand-System/PayLoad/Response/Class/ClassResponseV2.cs
@@ -6,6 +6,7 @@ namespace MagicLand_System.PayLoad.Response.Class
     public class ClassResponseV2 : ClassResponse
     {
         public string LecturerName { get; set; }
-        public List<string>? Schedules { get; set; }
+        public List<DailySchedule>? Schedules { get; set; }
+        public string? CourseName {  get; set; }    
     }
 }
